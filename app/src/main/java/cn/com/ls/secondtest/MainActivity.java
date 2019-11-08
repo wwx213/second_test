c77ee86866d9f3fd6f24ce345c81d41c0e987fc8
wwx213 <wwx213@163.com>
2019-11-08 11:33:31 +0800

    Initial commit

---
+++ app/src/main/java/cn/com/ls/secondtest/MainActivity.java
@@ -0,0 +1,19 @@
+package cn.com.ls.secondtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.jdom2.Document;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Document doc = new Document();
+    }
+}
